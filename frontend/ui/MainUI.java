82c1590fa89b24cbe0d1bcfd15ce2ac7fbb8a034
Biarritz644 <216010414+Biarritz644@users.noreply.github.com>
2026-02-15 01:34:32 +0100

    Create MainUI.java

---
+++ frontend/ui/MainUI.java
@@ -0,0 +1,8 @@
+package frontend.ui;
+
+public class MainUI {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenue dans le système de gestion de bibliothèque");
+    }
+}
